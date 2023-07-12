29539ed66eebb9aebd73b2386f6207bee295bad0
Germano007 <germanomanoel007@gmail.com>
2023-07-12 15:08:38 -0300

    Alert, Confirm e Prompt

---
+++ aula12/js/index.js
@@ -0,0 +1,25 @@
+//  Alert,Confirm e Prompt  (Aula 21)
+
+/* • Alert 
+   server para alerta alguma coisa na tela, básicamente isso. */
+
+// alert('Bom Dia, Germano Manoel!'); // -> Esse é um atalio pro de baixo. 
+        // OU
+// window.alert('Olá Mundo!');
+
+/* • Confirm 
+   serve para um usuario confirmar se realmente ele quer fazer aquela ação ou não.
+   Nisso vai mostrar pra ele dois botões na tela um de CANCELA e outro de OK, caso ele aperte em cancelar vai gerar um valor BOOLEAN do tipo FALSE se ele clicar em OK vai gerar um valor TRUE. */
+
+// confirm('Tem certeza que dezeja apagar ?'); // -> Esse é um atalho pro de baixo.
+        // OU
+// window.confirm('Tem certeza que dezeja apagar ?');
+
+
+/* • Prompt 
+    Serve para tu pedir pra que o  usúario digite alguma coisa pra tu;.
+*/
+
+// prompt('Digite Alguma coisa:'); // -> Esse é um atalio pro de baixo. 
+     // OU
+// window.prompt('Ditige Alguma coisa:');
